7524b9403f6a085f5a225364a2ea2e4f06fc740d
Dheeraj-Manwani <dkmanwani2000@gmail.com>
2024-06-01 14:18:31 +0530

    role and id in session

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const font = Poppins({
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <main className="flex h-full flex-col items-center justify-center bg-gray-950">
       <div className="space-y-6 text-center">
         <h1
           className={cn(
@@ -18,9 +18,9 @@ export default function Home() {
             font.className
           )}
         >
-          🔒Auth
+          The Utlimate Auth
         </h1>
-        <p className="text-white text-lg">A simple authentication service</p>
+        {/* <p className="text-white text-lg">A simple authentication service</p> */}
         <div>
           <LoginButton>
             <Button variant={"secondary"} size={"lg"}>
---
+++ auth.ts
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { getUserById } from "./data/user";
+
+import { db } from "@/lib/db";
+import authConfig from "@/auth.config";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      console.log({ sessiontoken: token });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
